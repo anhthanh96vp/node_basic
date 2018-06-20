3233868a46282b44553ab006fcd08631e0857994
cuong <nguyenvietcuong91hn@gmail.com>
2018-06-20 13:46:31 +0700

    Ngủ

--- exercise/uploadfile/server.js
+++ exercise/uploadfile/server.js
@@ -1,41 +1,28 @@
-var http = require("http");
-var fs = require("fs");
-var formidable = require("formidable");
+const express = require('express');
+const multer = require('multer');
+const app = express();
 
-http
-  .createServer(function(req, res) {
-    //Request đến url xem link có phải là /upload không
-    //Và request method có bằng post không
-    //Mặc định phương thức method là get chỉ khi upload file thì thành post
-    if (req.url == "/upload" && req.method.toLowerCase() == "post") {
-      // Khởi tạo form bằng 1 phương thức |đéo biết là cái đéo gì của formidable|
-
-      var form = new formidable.IncomingForm();
-
-      //Thiết lập thư mục chứa file trên server
-      form.uploadDir = "uploads/";
-
-      form.parse(req, function(err, fields, file) {
-        //path tmp trên server
-        var path = file.files.path;
-
-        //thiết lập path mới cho file
-        var newpath = form.uploadDir + file.files.name;
-
-        fs.rename(path, newpath, function(err) {
-          if (err) throw err;
-          res.end("Upload Thành Công!");
-        });
-      });
-      return;
+// diskStorage cần 2 thuộc tính là destination và filename
+const storage = multer.diskStorage({
+    destination: (req, file, callback) =>{
+        callback(null, '.')
+    },
+    filename: (req, file, callback) => {
+        callback(null, file.originalname);
     }
+})
 
-    res.writeHead("200", { "content-type": "text/html" });
-    fs.readFile("index.html", "utf8", function(err, data) {
-      if (err) throw err;
-      else console.log("Server running");
-      res.write(data);
-      res.end();
-    });
-  })
-  .listen(8888);
+// Thằng multer được truyền vào storage bằng với storage bên trên
+const upload = multer({storage: storage}).single('myfile');
+
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/upload', (req, res)=>{
+    upload(req, res), (err)=>{
+        if(err){return res.end('Error')}
+        
+        res.end('Upload file finish')
+    }
+})
